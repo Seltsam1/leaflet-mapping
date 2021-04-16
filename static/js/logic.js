2c945a9a9f7b326cd17bb04073bfee33a191417e
Brie Pfisterer <75340215+Seltsam1@users.noreply.github.com>
2021-04-16 16:27:05 -0700

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,17 @@
 // Leaflet mapping
 
-console.log("Testing")
+// Creating map object
+let myMap = L.map("map", {
+    center: [40.7608, -111.8910],
+    zoom: 6
+  });
+
+// Adding tile layer to the map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/light-v10",
+    accessToken: API_KEY
+  }).addTo(myMap);
