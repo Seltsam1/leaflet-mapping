6c33bf28851e0a267521289d6d53538800d578d8
Brie Pfisterer <75340215+Seltsam1@users.noreply.github.com>
2021-04-16 18:25:59 -0700

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -60,13 +60,61 @@ let overlays = {
 // User control for layers
 L.control.layers(baseMaps, overlays).addTo(myMap)
 
-// // API queries for data
-// let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+// Url for the API queries for data
+let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// // get json data from url with d3
-// d3.json(url).then(function(response) {
-//     console.log(response)
+// get json data from url with d3
+d3.json(url).then(function(response) {
+    // console log response data
+    console.log(response)
+    // function for marker style for earthquake data
+    function markerStyle(feature) {
+      return {
+        opacity: 1,
+        fillOpacity: 1,
+        fillColor: getColor(feature.properties.coordinates[2]),
+        color: "#000000",
+        radius: getRadius(feature.properties.mag),
+        stroke: true,
+        weight: 0.5
+      }
+    }
+    function getColor(depth) {
+      if(depth > 90) {
+        return "#ea2c2c";
+      } else if(depth > 70) {
+        return "#ea822c" 
+      } else if(depth > 50) {
+        return "#ee9c00"
+      } else if(depth > 30) {
+        return "eecc00"
+      } else if(deptj > 10) {
+        return "#d4ee00"
+      } else {
+        return "#98ee00"
+      }
+    }
+    // if mag is zero, returns 1, then scale value by 4 so easier to see on map
+    function getRadius(magnitude) {
+      if (magnitude === 0) {
+        return 1;
+      }
+      return magnitude * 4;
+    }
 
+    // create geojson data for markers
+    L.geoJson(response, {
+      pointToLayer: function(feature, latlng) {
+        console.log(response);
+        return L.circleMarker(latlng)
+      },
+      style: markerStyle,
+      onEachFeature: function
+    })
+
+
+
+});
 
 
 //   // array for markers
