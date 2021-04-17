f925e42b8b9321e1ca3b6bf5bb7855d42ce0c320
Brie Pfisterer <75340215+Seltsam1@users.noreply.github.com>
2021-04-16 17:07:55 -0700

    Update logic.js
    Markers in progress

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,20 +1,21 @@
 // Leaflet mapping
 
-// Creating map object
-let myMap = L.map("map", {
-    center: [40.7608, -111.8910],
-    zoom: 6
-  });
-
 // Adding tile layer to the map
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+let lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/light-v10",
     accessToken: API_KEY
-  }).addTo(myMap);
+  });
+
+// Creating map object
+let myMap = L.map("map", {
+  center: [40.7608, -111.8910],
+  zoom: 6,
+  layers: [lightmap, quakeLayer]
+});
 
 // API queries for data
 let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
@@ -22,4 +23,32 @@ let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.ge
 // get json data from url with d3
 d3.json(url).then(function(response) {
     console.log(response)
+
+  // array for markers
+  var quakeMarkers = [];
+
+  // loop through json data to store coordinates for markers
+//   for (let i = 0; i < response.length; i++) {
+// //     console.log(response[i].features.geometry.coordinates[0]);
+// //     console.log(response[i].features.properties.mag);
+// //   };
+// // });
+
+
+  for (let i = 0; i < response.length; i++) {
+    quakeMarkers.push(
+      L.circle(response[i].features.geometry.coordinates, {
+        stroke: false,
+        fillOpacity: 0.75,
+        color: "green",
+        fillColor: "green",
+        radius: (response[i].features.properties.mag)
+      })
+    );
+  };
 });
+
+// base layer for markers
+let baseMap = lightmap
+let quakeLayer = L.layerGroup(quakeMarkers);
+
